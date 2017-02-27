b220528fcaa58396ec6f66535479a801bed31904
Mike Letellier <mikeletellier@Bernards-MacBook-Pro.local>
2017-02-27 14:53:04 -0500

    attempting to make optimizations

--- confetti.js
+++ confetti.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
   AppRegistry,
   StyleSheet,
@@ -6,39 +6,41 @@ import {
   View,
   Animated,
   Dimensions
-} from 'react-native';
+} from 'react-native'
 
-let windowHeight = Dimensions.get('window').height;
-let windowWidth = Dimensions.get('window').width;
+let windowHeight = Dimensions.get('window').height
+let windowWidth = Dimensions.get('window').width
 
 class Confetti extends Component {
   constructor(props) {
-      super(props);
-      this._yAnimation = new Animated.Value(0);
-      this.color = this.randomColor();
-      this.left = this.randomValue(0, windowWidth);
+      super(props)
+      this._yAnimation = new Animated.Value(0)
+      this.color = this.randomColor()
+      this.left = this.randomValue(0, windowWidth)
+
+      this.style = props.index % 5 === 0 ? styles.smallConfetti : styles.bigConfetti
   }
 
   componentWillMount() {
-      let rotationOutput = this.randomValue(-220, 220) + 'deg';
+      let rotationOutput = this.randomValue(-220, 220) + 'deg'
       this._rotateAnimation = this._yAnimation.interpolate({
         inputRange: [0, windowHeight / 2, windowHeight],
         outputRange: ['0deg', rotationOutput, rotationOutput]
-      });
+      })
 
-      let xDistance = this.randomIntValue((windowWidth / 3 * -1), windowWidth / 3);
+      let xDistance = this.randomIntValue((windowWidth / 3 * -1), windowWidth / 3)
       this._xAnimation = this._yAnimation.interpolate({
         inputRange: [0, windowHeight],
         outputRange: [0, xDistance]
-      });
+      })
   }
 
   componentDidMount() {
-      let {duration, index} = this.props;
-        Animated.timing(this._yAnimation, {
-           duration: duration,
-           toValue: windowHeight + 1.25
-        }).start(this.props.onComplete);
+      let {duration, index} = this.props
+      Animated.timing(this._yAnimation, {
+          duration: duration,
+          toValue: windowHeight + 1.25
+      }).start(this.props.onComplete)
   }
 
   getTransformStyle() {
@@ -51,33 +53,12 @@ class Confetti extends Component {
       }
   }
 
-  getConfettiStyle() {
-      let {index} = this.props;
-      let bigConfetti = {
-        height: 5.5,
-        width: 11,
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
-        borderTopLeftRadius: 2.6,
-        borderTopRightRadius: 2.6
-      };
-      let smallConfetti = {
-        height: 4.5,
-        width: 8,
-        borderBottomLeftRadius: 2.5,
-        borderBottomRightRadius: 2.5,
-        borderTopLeftRadius: 1.3,
-        borderTopRightRadius: 1.3
-      }
-      return index % 5 === 0 ? smallConfetti : bigConfetti;
-  }
-
   randomValue(min, max) {
-      return Math.random() * (max - min) + min;
+      return Math.random() * (max - min) + min
   }
 
   randomIntValue(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
+      return Math.floor(Math.random() * (max - min) + min)
   }
 
   randomColor() {
@@ -87,13 +68,13 @@ class Confetti extends Component {
         "rgb(122.4, 198.9, 163.2)",
         "rgb(76.5, 193.8, 216.7)",
         "rgb(147.9, 99.4, 140.2)"
-      ];
-      return colors[this.randomIntValue(0,5)];
+      ]
+      return colors[this.randomIntValue(0,5)]
   }
 
   render() {
-      let {left, ...otherProps} = this.props;
-      return <Animated.View style={[styles.confetti, this.getConfettiStyle(), this.getTransformStyle(), {marginLeft: this.left, backgroundColor: this.color}]} {...otherProps}/>
+      let {left, ...otherProps} = this.props
+      return <Animated.View style={[styles.confetti, this.style, this.getTransformStyle(), {marginLeft: this.left, backgroundColor: this.color}]} {...otherProps}/>
   }
 }
 
@@ -105,7 +86,23 @@ const styles = StyleSheet.create({
   confetti: {
     position: 'absolute',
     marginTop: 0
+  },
+  bigConfetti: {
+    height: 5.5,
+    width: 11,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    borderTopLeftRadius: 2.6,
+    borderTopRightRadius: 2.6
+  },
+  smallConfetti: {
+    height: 4.5,
+    width: 8,
+    borderBottomLeftRadius: 2.5,
+    borderBottomRightRadius: 2.5,
+    borderTopLeftRadius: 1.3,
+    borderTopRightRadius: 1.3
   }
-});
+})
 
-export default Confetti;
+export default Confetti
--- confettiView.js
+++ confettiView.js
@@ -1,56 +1,69 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
   AppRegistry,
   StyleSheet,
   View,
   Animated
-} from 'react-native';
-
-import Confetti from "./confetti.js";
+} from 'react-native'
+import Confetti from "./confetti.js"
+import TimerMixin from 'react-timer-mixin'
+var reactMixin = require('react-mixin')
 
 class ConfettiView extends Component {
   constructor(props) {
-      super(props);
-      this.state = {confettis: []};
-      this.confettiIndex = 0;
+    super(props)
+    
+    this.state = {
+      confettis: []
+    }
+
+    this.confettiIndex = 0
   }
 
   startConfetti() {
-       let {confettis} = this.state;
-       let {confettiCount, timeout} = this.props;
-       if(this.confettiIndex < confettiCount) {
-         this.t = setTimeout(() => {
-           confettis.push({key: this.confettiIndex});
-           this.confettiIndex++;
-           this.setState({confettis});
-           this.startConfetti();
-         }, timeout);
-       }
+    let { confettis } = this.state
+    let { confettiCount, timeout } = this.props
+
+    if(this.confettiIndex < confettiCount) {
+      this.setTimeout(() => {
+        confettis.push({key: this.confettiIndex})
+        this.confettiIndex++
+        this.setState({confettis})
+        this.startConfetti()
+      }, timeout)
+    }
   }
 
   stopConfetti() {
-    clearTimeout(this.t)
+    this.clearTimeout()
   }
 
   removeConfetti(key) {
-       let {confettis} = this.state;
-       let {confettiCount} = this.props;
-       let index = confettis.findIndex(confetti => {return confetti.key === key});
-       confettis.splice(index, 1);
-       this.setState({confettis});
-       if(key === confettiCount - 1) {
-         this.confettiIndex = 0;
-       }
+    let { confettis } = this.state
+    let { confettiCount } = this.props
+
+    let index = confettis.findIndex(confetti => {
+      return confetti.key === key
+    })
+
+    confettis.splice(index, 1)
+
+    this.setState({
+      confettis
+    })
   }
 
   render() {
-       let {confettis} = this.state;
-       let {...otherProps} = this.props
-       return <View style={styles.container}>
-         {confettis.map(confetti => {
-             return <Confetti key={confetti.key} index={confetti.key} onComplete={this.removeConfetti.bind(this, confetti.key)} {...otherProps}/>
-         })}
-       </View>
+    let { confettis } = this.state
+    let { ...otherProps } = this.props
+    
+    return (
+      <View style={styles.container}>
+        {confettis.map(confetti => {
+          return <Confetti key={confetti.key} index={confetti.key} onComplete={this.removeConfetti.bind(this, confetti.key)} {...otherProps}/>
+        })}
+      </View>
+    )
   }
 }
 
@@ -66,6 +79,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0
   }
-});
+})
 
-export default ConfettiView;
+reactMixin(ConfettiView.prototype, TimerMixin)
+
+export default ConfettiView
